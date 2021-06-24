ccfaa3fbb4cef26e8e283533b7182e1ff84a3445
Nobumichi Leo Nakamura <we.are.one779@gmail.com>
2021-06-24 21:42:34 +0900

    Added test case

--- api/todos/tests.py
+++ api/todos/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Todo
+
+
+class TodoModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Todo.objects.create(title='Test title', body='Test body')
+
+    def test_title_content(self):
+        todo = Todo.objects.get(id=1)
+        expected_object_name = f'{todo.title}'
+        self.assertEqual(expected_object_name, 'Test title')
+
+    def test_body_content(self):
+        todo = Todo.objects.get(id=1)
+        expected_object_name = f'{todo.body}'
+        self.assertEqual(expected_object_name, 'Test body')
